aa4733c4a42b78e45d31e9445b2b43cba5bd2deb
bryanjonker <jonker@illinois.edu>
2020-10-20 15:54:13 -0500

    Add project files.

---
+++ Server/Controllers/StoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace halloween_story.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StoryController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<StoryController> logger;
+
+        public StoryController(ILogger<StoryController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => Summaries[rng.Next(Summaries.Length)]).ToArray();
+        }
+    }
+}
